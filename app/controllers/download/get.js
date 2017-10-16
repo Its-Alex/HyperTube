2a05a4d51b2ef917af0b670a3f91485ddc570bf4
Alexandre MARRE <xSkyZie@gmail.com>
2017-10-16 17:25:04 +0200

    Push download v1

--- app/controllers/download/get.js
+++ app/controllers/download/get.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 function error (res, error, status) {
   res.status(status)
   res.json({
@@ -7,5 +9,47 @@ function error (res, error, status) {
 }
 
 module.exports = (req, res) => {
-  res.json({success: true})
+  let file = './public/toystory.mp4'
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      // If file no exist
+      if (err.code === 'ENOENT') {
+        return res.sendStatus(404)
+      }
+    }
+
+    // Get range obtain by browser
+    let range = req.headers.range
+
+    // Check if navigator ask for a range
+    if (!range) {
+      console.log(new Error('Wrong range'))
+      return error(res, 'Wrong range', 416)
+    }
+    // Convert range into array
+    let positions = range.replace(/bytes=/, '').split('-')
+
+    let start = parseInt(positions[0], 10)
+    let end = positions[1] ? parseInt(positions[1], 10) : stats.size - 1
+    let chunksize = (end - start) + 1
+    let head = {
+      'Content-Range': 'bytes ' + start + '-' + end + '/' + stats.size,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4'
+    }
+    res.writeHead(206, head)
+    let stream = fs.createReadStream(file, {
+      start: start,
+      end: end
+    })
+    stream.on('open', () => {
+      stream.pipe(res)
+    })
+    stream.on('error', (err) => {
+      console.log(err)
+      return error(res, 'Internal server error', 500)
+    })
+  })
 }
