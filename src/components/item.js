4b47adac380452e74566bd81043c8e2458cf8f69
Tac&Tic <vacrozet@student.42.fr>
2017-10-23 14:47:38 +0200

    rectif

--- src/components/item.js
+++ src/components/item.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import { Item, Label } from 'semantic-ui-react'
 import '../scss/item.css'
+import axios from 'axios'
 
 class Iteme extends Component {
   constructor (props) {
@@ -9,32 +10,49 @@ class Iteme extends Component {
 
     }
   }
+  
+  componentWillMount () {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=4add767f00472cadffc84346bd8572e6&query=Jack+Reacher').then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+  
 
   render () {
     return (
       <div>
         <div className='bodytest'>
-          <div className='content'>
+          <div className='container'>
             <div className='imgcontent'>
               <img src='https://www.ecranlarge.com/uploads/image/000/963/full-metal-jacket-affiche-963183.jpg' alt='Girl in a jacket' />
             </div>
-            <div>
-              coucou
+            <div className='description'>
+              <div className='titre'>
+                la je met le titre
+              </div>
+              <div className='resumer'>
+                lale resumer resumer
+              </div>
+              <div className='note'>
+                la la note
+              </div>
             </div>
           </div>
-          <div className='content'>
+          <div className='container'>
             <div className='inside'>
               <div className='imgcontent'>
                 <img src='https://www.ecranlarge.com/uploads/image/000/963/full-metal-jacket-affiche-963183.jpg' alt='Girl in a jacket' />
               </div>
             </div>
           </div>
-          <div className='content'>
+          <div className='container'>
             <div className='imgcontent'>
               <img src='https://www.ecranlarge.com/uploads/image/000/963/full-metal-jacket-affiche-963183.jpg' alt='Girl in a jacket' />
             </div>
           </div>
-          <div className='content'>
+          <div className='container'>
             <div className='imgcontent'>
               <img src='https://www.ecranlarge.com/uploads/image/000/963/full-metal-jacket-affiche-963183.jpg' alt='Girl in a jacket' />
             </div>
