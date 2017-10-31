279f54602f81439f4ffec40dec80df08e4e034a9
Alexandre MARRE <xSkyZie@gmail.com>
2017-10-31 11:47:05 +0100

    Update middleware

--- app/utils/middleware.js
+++ app/utils/middleware.js
@@ -10,20 +10,20 @@ function error (res, error, status) {
 
 module.exports = () => {
   return (req, res, next) => {
-    var auth = req.get('Authorization')
+    var auth = req.get('Authorization') || req.query.Authorization
 
     if (auth === undefined) {
       return error(res, 'Need Authorization in header', 401)
     }
-    auth = auth.split(' ')
-    if (auth[0] !== 'Bearer' || auth[1].length !== 128 || auth.length !== 2) {
+    auth = auth.replace('Bearer ', '')
+    if (auth.length !== 128) {
       return error(res, 'Wrong authorization header', 401)
     }
 
-    model.getUserByToken(auth[1]).then(results => {
+    model.getUserByToken(auth).then(results => {
       if (results.length <= 0) return error(res, 'Wrong token', 401)
       req.user = {
-        token: auth[1],
+        token: auth,
         id: results[0].userId,
         mail: results[0].mail,
         username: results[0].username,
