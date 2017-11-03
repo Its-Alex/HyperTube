f04777e5f81ca3ff58973974f7449be47150a9d3
Tac&Tic <vacrozet@student.42.fr>
2017-11-03 18:14:49 +0100

    lecture et comment

--- src/routes/lecture.js
+++ src/routes/lecture.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import Player from '../components/player.js'
+import Comment from '../components/comment.js'
 
-class componentName extends Component {
+class Lecture extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
   render () {
     return (
       <div>
-        
+        <Player />
+        <Comment id={this.props.match.params.id} />
       </div>
     )
   }
 }
 
-export default componentName
+export default Lecture
