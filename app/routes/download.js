16dcf52d1d39127e91f1f70903564aa141a39917
ItsAlex <me@itsalex.fr>
2017-11-03 20:02:12 +0100

    Delete route

--- app/routes/download.js
+++ app/routes/download.js
@@ -3,6 +3,5 @@ const router = express.Router()
 
 router.get('/:id', require('../utils/middleware.js')(), require('../controllers/download/get.js'))
 router.post('/:id', require('../utils/middleware.js')(), require('../controllers/download/post.js'))
-router.delete('/', require('../utils/middleware.js')(), require('../controllers/download/delete.js'))
 
 module.exports = router
