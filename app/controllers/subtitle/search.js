48e8688fc2cebd4922e981b074534c6748a3977b
ItsAlex <me@itsalex.fr>
2017-11-03 16:29:34 +0100

    Search on subtitle done

--- app/controllers/subtitle/search.js
+++ app/controllers/subtitle/search.js
@@ -37,9 +37,9 @@ module.exports = (req, res) => {
 
   let lng
   if (req.query.lang.length === 2) {
-    lng = langs.where(1, req.query.lang)['2B']
+    lng = langs.where(1, req.query.lang)
   } else if (req.query.lang.length === 3) {
-    lng = langs.where(2, req.query.lang)['2B']
+    lng = langs.where(2, req.query.lang)
   } else {
     return error(res, 'Invalid lang', 403)
   }
@@ -56,67 +56,87 @@ module.exports = (req, res) => {
     if (result.length === 0) return error(res, 'No movie for this id', 403)
     if (result[0].state === 'search') return error(res, 'Need to download movie first', 403)
     let file = result[0]
-    let timeout = true
-    let timer = setTimeout(() => {
-      timeout = false
-      error(res, 'Server timeout', 408)
-    }, 10000)
 
-    OpenSubtitles.search({
-      sublanguageid: lng,
-      filesize: file.length,
-      path: file.originalPath,
-      filename: path.parse(file.originalPath).base,
-      extensions: ['srt', 'vtt'],
-      limit: 'all',
-      imdbid: file.imdbId
-    }).then(subtitles => {
-      if (timeout) {
-        clearTimeout(timer)
-        each(subtitles[lng.substr(0, 2)], (sub, cb) => {
-          sub.movieId = req.params.id
-          sub.uuid = genUuid()
-          sub.path = global.config.pathStorage + 'subtitles/' + sub.uuid + '.vtt'
-
-          axios.request({
-            responseType: 'arraybuffer',
-            url: sub.url,
-            method: 'get',
-            headers: {
-              'Content-Type': 'text/plain'
-            }
-          }).then((result) => {
-            if (path.extname(sub.filename) === '.srt') {
-              srt2vtt(result.data, (err, vttData) => {
-                if (err) {
-                  console.log(err)
-                  return cb(null)
-                }
-                fs.writeFileSync(sub.path, vttData)
-                addSub(sub, cb)
-              })
-            } else {
-              fs.writeFileSync(sub.path, result.data)
-              addSub(sub, cb)
-            }
-          }).catch(err => {
-            console.log(err)
-            cb(null)
-          })
-        }, err => {
-          if (err) {
-            console.log(err)
-            return error(res, 'Internal server error', 500)
-          }
-          res.json({
-            success: true
-          })
+    model.getFromMovieAndLang(file.id, lng['1']).then(result => {
+      if (result.length !== 0) {
+        return res.json({
+          success: true,
+          result
         })
       }
+
+      let timeout = true
+      let timer = setTimeout(() => {
+        timeout = false
+        error(res, 'Server timeout', 408)
+      }, 10000)
+
+      OpenSubtitles.search({
+        sublanguageid: lng['2B'],
+        filesize: file.length,
+        path: file.originalPath,
+        filename: path.parse(file.originalPath).base,
+        extensions: ['srt', 'vtt'],
+        limit: 'all',
+        imdbid: file.imdbId
+      }).then(subtitles => {
+        if (timeout) {
+          clearTimeout(timer)
+          each(subtitles[lng['1'].substr(0, 2)], (sub, cb) => {
+            sub.movieId = req.params.id
+            sub.uuid = genUuid()
+            sub.path = global.config.pathStorage + 'subtitles/' + sub.uuid + '.vtt'
+
+            axios.request({
+              responseType: 'arraybuffer',
+              url: sub.url,
+              method: 'get',
+              headers: {
+                'Content-Type': 'text/plain'
+              }
+            }).then((result) => {
+              if (path.extname(sub.filename) === '.srt') {
+                srt2vtt(result.data, (err, vttData) => {
+                  if (err) {
+                    console.log(err)
+                    return cb(null)
+                  }
+                  fs.writeFileSync(sub.path, vttData)
+                  addSub(sub, cb)
+                })
+              } else {
+                fs.writeFileSync(sub.path, result.data)
+                addSub(sub, cb)
+              }
+            }).catch(err => {
+              console.log(err)
+              cb(null)
+            })
+          }, err => {
+            if (err) {
+              console.log(err)
+              return error(res, 'Internal server error', 500)
+            }
+            model.getFromMovieAndLang(file.id, lng['1']).then(result => {
+              res.json({
+                success: true,
+                result
+              })
+            }).catch(err => {
+              console.log(err)
+              error(res, 'Internal server error', 500)
+            })
+          })
+        }
+      }).catch(err => {
+        console.log(err)
+        if (timeout) return error(res, 'Internal server error', 500)
+      })
     }).catch(err => {
       console.log(err)
-      if (timeout) return error(res, 'Internal server error', 500)
+      error(res, 'Internal server error', 500)
     })
+
   }).catch(err => {
     console.log(err)
     return error(res, 'Internal server error', 500)
